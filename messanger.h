3c7949325f8e3ddfa2655c5de37a9dea69ae1d02
mahan <mahan.mohammadi.a@gmail.com>
2025-05-30 15:22:30 +0330

    Add project files.

---
+++ messanger.h
@@ -0,0 +1,8 @@
+﻿// messanger.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+
+
+// TODO: Reference additional headers your program requires here.
---
+++ project3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "date.h"
+#include <string>
+#include "person.h"
+#include "encdec.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello world." << endl;
+	return 0;
+}
